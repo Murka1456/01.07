7db5ec7da2b3f8a5bfbad91f96e4aa08a18d1299
Murka1456 <mal-bod2@hotmail.com>
2025-07-01 11:09:50 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+console.log('Hello!');
+let x = 4;
+let y = 3;
+if (x < y) {
+  console.log(`´Tallet er mindre`);
+} else if (x == y) {
+  console.log(`Talet er like stor`);
+} else {
+  console.log(`Talet er større`);
+}
+
+let hour = 19;
+if (hour <= 4) {
+  console.log(`God natt`);
+} 
+else if (hour <10){
+  console.log(`God morgen`);
+} 
+else if (hour < 18){
+
+  console.log(`God kveld`)
+}
+else( hour> 18){
+  console.log(`God natt`)
+}
+const fruits = [`Eple`,`Appelsen`,`Pære`,`Ananas`];
+console.log(fruits);
+let favoritFruits = fruits.pop();
+console.log(fruits);
+console.log(favoritFruits);
+
+fruits.push(`Kiwi`);
+console.log(fruits);
+fruits.unshift(`Klementin`);``
+console.log(fruits);
+favoriteFriuts = fruits.shift();
+console.log(fruits);
+fruits.pop = (`morel`);
+favoritFruits = fruits.shift();
+console.log  (`Joining`);
+fruits.unshift = (favoritsFruit);
+console.log 
+console.log(`Joining`);
+console.log(fruits.join(``));
+let fruit = fruits.at(2);
+console.log(fruits);
+//Students
+const classA = [`Sigurd`, `Thomas`, `Bjørn`,` Emilie`];
+const classB = [ `Hannah`,`Silje`, `Per`];
+const allStidents = classA.concat(classB);
+console.log(classA);
+console.log(classB);
+console.log(allStudents);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
